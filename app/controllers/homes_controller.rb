91cb1db5c2859e0aa1aef430c5cd9689b1f193e0
gontamooooon <rika.gonta1014@gmail.com>
2022-03-26 09:25:33 +0000

    Rubocop

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,5 @@
 class HomesController < ApplicationController
   def top
-    @all_ranks = Book.find(Favorite.group(:book_id).order('count(book_id) desc').limit(3).pluck(:book_id))
+    @all_ranks = Book.find(Favorite.group(:book_id).order('count(book_id)desc').limit(3).pluck(:book_id))
   end
 end
