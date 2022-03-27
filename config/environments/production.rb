9303b4c6fc1fe44a9520360172c432beec4c01aa
gontamooooon <rika.gonta1014@gmail.com>
2022-03-27 14:54:05 +0000

    [修正]production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,6 +117,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.hosts << "35.73.121.235"
+  config.active_job.queue_adapter = :inline
   
   config.action_mailer.perform_caching = true
   config.action_mailer.default_url_options = {  host: '35.73.121.235' }
