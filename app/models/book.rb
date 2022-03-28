8b74ec59a74d9fcff8fe49a61f80cd8fac332eb5
gontamooooon <rika.gonta1014@gmail.com>
2022-03-28 10:57:16 +0000

    バリデーション修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ApplicationRecord
   has_one_attached :book_image
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  validates :title, :author, :genre, presence: true
+  validates :title, :author, :genre, :rate, presence: true
   validates :body, presence: true, length: { maximum: 500 }
 
   def get_book_image
